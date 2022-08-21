09e6af5d25a8df02d11ef0f0ae04b38a3811d27c
jjjkahn <31192156+jjjkahn@users.noreply.github.com>
2022-08-21 19:59:14 -0400

    Components Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShopingListComponent } from './Shopping/shoping-list/shoping-list.component';
+import { RecipeListComponent } from './Recipe/recipe-list/recipe-list.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipeItemComponent } from './Recipe/recipe-list/recipe-item/recipe-item.component';
+import { ShopingEditComponent } from './Shopping/shoping-list/shoping-edit/shoping-edit.component';
+import { RecipeDetailComponent } from './Recipe/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShopingListComponent,
+    RecipeListComponent,
+    RecipeComponent,
+    RecipeItemComponent,
+    ShopingEditComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
